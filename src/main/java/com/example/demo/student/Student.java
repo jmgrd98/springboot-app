fb36b3778427bc66225e717e0f721d3f5b5a0a6f
João Marcelo Dantas <jmgrd98@gmail.com>
2023-12-09 22:26:44 -0300

    Initial commit

---
+++ src/main/java/com/example/demo/student/Student.java
@@ -0,0 +1,11 @@
+package com.example.demo.student;
+
+import java.time.LocalDate;
+
+public class Student {
+    private Long id;
+    private String name;
+    private String email;
+    private Integer age;
+    private LocalDate dob;
+}
